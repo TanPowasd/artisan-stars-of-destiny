8d7e2db29a74fd09bc2041d5017e02d00ea61214
TanPowasd <wang1422857@outlook.com>
2025-10-16 00:09:45 +0800

    暂存

---
+++ src/main/java/org/tp/AsodF/MixinExtra/MinecraftPlayerTranscendentState.java
@@ -0,0 +1,13 @@
+package org.tp.AsodF.MixinExtra;
+
+public class MinecraftPlayerTranscendentState {
+    private static int MaxSolarEnergy=500;
+    private  float SolarEnergy=0f;
+
+    private static int MaxShadowEnergy=500;
+    private  float ShadowEnergy=0f;
+
+    private static int MaxTrancendentEnergy=1000;
+    private  float TrancendentEnergy=0f;
+
+}
---
+++ src/main/java/org/tp/asod/AsodMixin/AsodMinecraftPlayerEntityMixin.java
@@ -0,0 +1,10 @@
+package org.tp.asod.AsodMixin;
+
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(Player.class)
+public class AsodMinecraftPlayerEntityMixin {
+
+}
